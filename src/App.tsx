57cb9ef620963e26c6a69d8e9657cb7c7e8d9d83
adedamola-toye <toye.adedamola@gmail.com>
2024-08-21 19:16:57 +0100

    Fix blank page issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import WeatherPage from './Components/Weather/WeatherPage'
+import WeatherWiseAppProvider from './Components/WeatherWiseAppProvider'
 function App() {
 
   return (
+    <WeatherWiseAppProvider>
       <WeatherPage/>
+    </WeatherWiseAppProvider>
+      
 
   )
 }
